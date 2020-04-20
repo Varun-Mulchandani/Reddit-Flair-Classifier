469511fbebd8edf8c549d238ed9944b7a0d40ecf
Varun-Mulchandani <49016008+Varun-Mulchandani@users.noreply.github.com>
2020-04-20 15:30:18 +0530

    Add files via upload

--- final.py
+++ final.py
@@ -1,37 +1,44 @@
 from flask import Flask,render_template,url_for,request
 import urllib.request, json
-from sklearn.externals import joblib
 import os
-from tensorflow import set_random_seed
-from numpy.random import seed
+import pickle
+#from tensorflow import set_random_seed
+#from numpy.random import seed
 import pandas as pd
 import numpy as np
 import string, os
 import warnings
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import keras
+import numpy as np
+import pandas as pd
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.models import Sequential
+from keras.layers import Dense, Embedding, LSTM, SpatialDropout1D
+from keras.utils.np_utils import to_categorical
+from keras.callbacks import EarlyStopping
+from keras.layers import Dropout
 
+train = pd.read_csv('reddddddit.csv')
+max_no_word = 50000
+max_seq_length = 250
+embedding_dim = 100
 
-def generate_flair(sentence):
-  all_tokens = []
-  all_masks = []
-  all_segments = []
-  sentence = tokenizer.tokenize(sentence)
-  sentence = sentence[:160 - 2]
-  input_sequence = ['[CLS]'] + sentence + ['[SEP]']
-  pad_len = 160 - len(input_sequence)
-  tokens = tokenizer.convert_tokens_to_ids(input_sequence)
-  tokens += [0]* pad_len
-  pad_masks = [1] * len(input_sequence) + [0] * pad_len
-  segment_ids = [0] * 160
-  all_tokens.append(tokens)
-  all_masks.append(pad_masks)
-  all_segments.append(segment_ids)
-  input_s = (np.array(all_tokens), np.array(all_masks), np.array(all_segments))
-  out = model.predict(input_s)
-  out.argmax()
-  for name, ids in label_to_id.items():
-    if ids == (out.argmax()):
-      finaloutput = name
-  return finaloutput
+tokenizer = Tokenizer(num_words = max_no_word, filters = '!"#$%&()*+,-./:;<=>?@[\]^_`{|}~', lower = True)
+tokenizer.fit_on_texts(train['title'].values)
+list_classes = ["AskIndia", "Non-Political", "[R]eddiquette",
+                "Scheduled", "Photography", "Science/Technology",
+                "Politics", "Business/Finance", "Policy/Economy",
+                "Sports", "Food", "AMA"]
+def generate_flair(title):
+    title = [title]
+    seq = tokenizer.texts_to_sequences(title)
+    padded = pad_sequences(seq, maxlen = max_seq_length)
+    pred = model.predict(padded)
+    return list_classes[np.argmax(pred)]
 
 
 app = Flask(__name__)
@@ -40,12 +47,12 @@ def main():
     return render_template('main.html')
 @app.route('/predict',methods=['POST'])
 def predict():
-    set_random_seed(2)
-    seed(1)
+    global model
+    #set_random_seed(2)
+    #seed(1)
     warnings.filterwarnings("ignore")
     warnings.simplefilter(action = 'ignore',category = FutureWarning)
-    Classifier_bert = open('Classifier_bert.pkl','rb')
-    model = joblib.load(Classifier_bert)
+    model = pickle.load(open('Classifier_bert.pkl','rb'))
 
     if request.method == 'POST':
         message = request.form['message']
@@ -54,8 +61,8 @@ def predict():
             data = json.loads(url.read().decode())
         title = data[0]['data']['children'][0]['data']['title']
         my_prediction = generate_flair(title)
-        #print(my_prediction)
-        #my_prediction = model.predict()
+#print(my_prediction)
+#my_prediction = model.predict()
     return render_template('mainresult.html',prediction = my_prediction)
 
 if __name__ == '__main__':
